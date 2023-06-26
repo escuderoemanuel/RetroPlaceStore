0c2bf84db043b98f069aec520f02898f122a0776
Emanuel Escudero <104809207+escuderoemanuel@users.noreply.github.com>
2023-06-26 12:22:48 -0300

    Update ItemFooterDev.jsx

--- src/components/ItemFooterDev/ItemFooterDev.jsx
+++ src/components/ItemFooterDev/ItemFooterDev.jsx
@@ -5,19 +5,19 @@ import './FooterDev.css';
 export const ItemFooterDev = () => {
   return (
     <div className='footerDeveloper'>
-      
+      <p> • Desarrollado por Emanuel Escudero •</p>
       <a
         className='developerLink'
         href='https://emanuelescudero.ar'
         target='_blank'
         rel='noopener noreferrer'>
-        • Desarrollado por Emanuel Escudero •
-      </a>
-      <img
+        <img
         className='logoDev'
         src='https://i.ibb.co/ZT13Snc/Logo2023.png'
         alt='Logo del Desarrollador'
       />
+      </a>
+      
     </div>
   );
 };
